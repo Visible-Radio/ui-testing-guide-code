3e7979fd973d215249e0f06bd8a93661a961b73d
Patrick Kaipainen <patrick.kaipainen@gmail.com>
2024-03-19 16:22:43 -0400

    add axe-playwright to check for accessibility issues during testing

--- .storybook/main.js
+++ .storybook/main.js
@@ -6,6 +6,10 @@ const config = {
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
+    // should enable this - but it is crashing the build for some reason
+    // this is for manual checks
+    // '@storybook/addon-a11y',
+    // we'll also use axe-playwright to programatically check for accessibility issues during testing
   ],
   framework: {
     name: "@storybook/react-vite",
